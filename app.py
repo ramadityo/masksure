7ac188680cc3b944bfd88228bdd107dc9d879557
_userNull <ramzprivatexd@gmail.com>
2024-12-08 08:20:50 +0700

    add: streamlit

--- app.py
+++ app.py
@@ -1,25 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import os
-from PIL import Image, ImageEnhance
-import subprocess
+import streamlit as st
+from PIL import Image
+from detection import Detection
+import matplotlib.pyplot as plt
+import pandas as pd
 
-app = Flask(__name__, static_folder="./static", template_folder="./templates")
+st.set_page_config(page_title="Object Detection Web App - Viskom")
 
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+st.write(
+    """
+    # Object Detection Web App
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    Selamat datang di Aplikasi Object Detection sederhana! 🚀 Aplikasi ini dikhususkan untuk memenuhi
+    syarat Ujian Akhir Semester dari Visi Komputer. Aplikasi ini dibuat untuk mendeteksi orang
+    yang memakai masker maupun tidak. Selamat mencoba!
+    """
+)
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# st.image("assets/20241128_214330_masksure.png")
 
+UPLOAD_IMAGE = st.file_uploader("Choose a file")
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    
-    return render_template('home.html')
+if UPLOAD_IMAGE is not None:
+    img = Image.open(UPLOAD_IMAGE)
+    img = img.resize((img.width // 2, img.height // 2))
+    st.image(img)
 
+    if st.button("Deteksi gambar!"):
+        img_result, stats = Detection.detect(img)
+        st.image(img_result)
 
-if __name__ == '__main__':  
-    app.run()
+        st.write("## Statistik 📈")
+
+        PREPROCESS_MILISEC = float(stats['preprocess']) * 1000
+        PREPROCESS_MILISEC = round(PREPROCESS_MILISEC / 1000, 1)
+
+        INFERENCE_MILISEC = float(stats['inference']) * 1000
+        INFERENCE_MILISEC = round(INFERENCE_MILISEC / 1000, 1)
+
+        POSTPROCESS_MILISEC = float(stats['postprocess']) * 1000
+        POSTPROCESS_MILISEC = round(POSTPROCESS_MILISEC / 1000, 1) 
+
+        data = {
+            'Stage': ['Preprocess', 'Inference', 'Postprocess'],
+            'Time (ms)': [PREPROCESS_MILISEC, INFERENCE_MILISEC, POSTPROCESS_MILISEC]
+        }
+
+        df = pd.DataFrame(data)
+        st.table(df)
+        df = df.set_index('Stage')
+
+        st.area_chart(df)
+
--- detection.py
+++ detection.py
@@ -1,17 +1,16 @@
-# import PIL 
+
 from PIL import Image
-import matplotlib.pyplot as plt
-# import ultralytics
 from ultralytics import YOLO
 
 class Detection:
 
-    def detect(file_path):
-        img = Image.open(file_path)
+    def detect(img):
+        # img = Image.open(file_path)
         model = YOLO('model/last.pt')
         results = model(img)
 
         rgb_image = results[0].plot()[:, :, ::-1]
         Image.fromarray(rgb_image).save('result.png')
         img_result = Image.open('result.png')
-        return img_result
+        results = results[0].speed
+        return img_result, results
