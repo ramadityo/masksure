cbcf90f614f56bb9f2d79626aacaa0c4db390f42
_userNull <ramzprivatexd@gmail.com>
2024-12-08 08:25:42 +0700

    fix: error in app.py

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ from detection import Detection
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from PIL import Image
+from ultralytics import YOLO
+
+class Detection:
+
+    def detect(img):
+        # img = Image.open(file_path)
+        model = YOLO('model/last.pt')
+        results = model(img)
+
+        rgb_image = results[0].plot()[:, :, ::-1]
+        Image.fromarray(rgb_image).save('result.png')
+        img_result = Image.open('result.png')
+        results = results[0].speed
+        return img_result, results
+
 st.set_page_config(page_title="Object Detection Web App - Viskom")
 
 st.write(
--- detection.py
+++ detection.py
@@ -1,16 +1,2 @@
 
-from PIL import Image
-from ultralytics import YOLO
 
-class Detection:
-
-    def detect(img):
-        # img = Image.open(file_path)
-        model = YOLO('model/last.pt')
-        results = model(img)
-
-        rgb_image = results[0].plot()[:, :, ::-1]
-        Image.fromarray(rgb_image).save('result.png')
-        img_result = Image.open('result.png')
-        results = results[0].speed
-        return img_result, results
